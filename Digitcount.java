e0827b88188dccfe2c11533f2a9f6c4a0e66c0ce
Gurujayasri G <gurujayasri33@gmail.com>
2026-01-28 11:06:32 +0530

    Add files via upload

---
+++ Digitcount.java
@@ -0,0 +1,20 @@
+package PST;
+import java.util.Scanner;
+
+public class Digitcount {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num, count = 0;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println("Digit count = " + count);
+    }
+}
