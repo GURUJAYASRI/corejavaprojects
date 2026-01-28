34f48c735fe9f60c0ce3816f665ab5b180b94a72
Gurujayasri G <gurujayasri33@gmail.com>
2026-01-28 11:09:49 +0530

    Add files via upload

---
+++ Sumofdigits.java
@@ -0,0 +1,20 @@
+package PST;
+import java.util.Scanner;
+
+public class Sumofdigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num, sum = 0;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        while (num > 0) {
+            sum = sum + (num % 10); 
+            num = num / 10;         
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
